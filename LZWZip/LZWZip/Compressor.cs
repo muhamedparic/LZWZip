060fdca996bf543a07189d46fcc3a215467ba4e0
muhamedparic <mparic1@etf.unsa.ba>
2018-01-07 15:34:25 +0100

    Initial project

---
+++ LZWZip/LZWZip/Compressor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lzw
+{
+    public enum CompressionMode
+    {
+        CompressThenJoin,
+        JoinThenCompress
+    }
+
+    public class Compressor
+    {
+        private List<FileStream> files = new List<FileStream>();
+        public List<FileStream> Files
+        {
+            get
+            {
+                return files;
+            }
+        }
+
+        public uint MaxSymbolLength { get; set; }
+        public CompressionMode FileCompressionMode { get; set; } = CompressionMode.CompressThenJoin;
+
+        public MemoryStream Run()
+        {
+            return null;
+        }
+    }
+}
